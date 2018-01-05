e2a0de17f8f9d567945bae48a00f5cc99da70c84
coon <coon@mailbox.org>
2018-01-05 23:13:22 +0100

    USE SPI3 port

--- hal/stm32f4/hal_stm32f4.c
+++ hal/stm32f4/hal_stm32f4.c
@@ -26,12 +26,12 @@ uint8_t getCe() {
 }
 
 uint8_t spiXmitByte(uint8_t value) {
-	SPI1->DR = value; // write data to be transmitted to the SPI data register
-	while( !(SPI1->SR & SPI_I2S_FLAG_TXE) ); // wait until transmit complete
-	while( !(SPI1->SR & SPI_I2S_FLAG_RXNE) ); // wait until receive complete
-	while( SPI1->SR & SPI_I2S_FLAG_BSY ); // wait until SPI is not busy anymore
+	SPI3->DR = value; // write data to be transmitted to the SPI data register
+	while( !(SPI3->SR & SPI_I2S_FLAG_TXE) ); // wait until transmit complete
+	while( !(SPI3->SR & SPI_I2S_FLAG_RXNE) ); // wait until receive complete
+	while( SPI3->SR & SPI_I2S_FLAG_BSY ); // wait until SPI is not busy anymore
 
-	return SPI1->DR; // return received data from SPI data register
+	return SPI3->DR; // return received data from SPI data register
 }
 
 void spiInit() {
@@ -39,29 +39,29 @@ void spiInit() {
 	SPI_InitTypeDef SPI_InitStruct;
 
 	// enable clock for used IO pins
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
 
-	/* configure pins used by SPI1
-	 * PA5 = SCK
-	 * PA6 = MISO
-	 * PA7 = MOSI
-	 */
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_7 | GPIO_Pin_6 | GPIO_Pin_5;
+	// configure pins used by SPI3
+	// PC10 = SCK
+	// PC11 = MISO
+	// PC12 = MOSI 
+
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11 | GPIO_Pin_12;
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
 	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-	// connect SPI1 pins to SPI alternate function
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource5, GPIO_AF_SPI1);
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource6, GPIO_AF_SPI1);
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource7, GPIO_AF_SPI1);
+	// connect SPI3 pins to SPI alternate function
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF_SPI3);
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource11, GPIO_AF_SPI3);
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource12, GPIO_AF_SPI3);
 
 	// enable peripheral clock
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI3, ENABLE);
 
-	/* configure SPI1 in Mode 0
+	/* configure SPI3 in Mode 0
 	 * CPOL = 0 --> clock is low when idle
 	 * CPHA = 0 --> data is sampled at the first edge
 	 */
@@ -75,7 +75,7 @@ void spiInit() {
 	SPI_InitStruct.SPI_FirstBit = SPI_FirstBit_MSB;// data is transmitted MSB first
 	SPI_Init(SPI1, &SPI_InitStruct);
 
-	SPI_Cmd(SPI1, ENABLE); // enable SPI1
+	SPI_Cmd(SPI3, ENABLE); // enable SPI3
 	_init_TIM3(); // enable Timer 3 for delay function
 }
 
